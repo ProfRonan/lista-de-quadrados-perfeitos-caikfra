d2556f4a2b4ae0955d55145fb9a152d7d976a127
Caik Franco <128155208+caikfra@users.noreply.github.com>
2023-04-18 15:54:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-print("Quantos primos?")
-F = int(input("> "))
-O = F
-N = 2
-i = 1
-Lista = []
-if F <=0:
-    print("Insira um número maior")
-if F > 0:
-    while True:
-        i = i + 1
-        if N % i !=0:
-            i = i + 1
-        if N % i ==0 and i !=N:
-            N = N + 1
-            i = 1
-        if N % i ==0 and i ==N:
-            F = F - 1
-            i = 1
-            Lista.append(N)
-            if F > 0:
-                N = N + 1
-            if F <=0:
-                F = O
-                print((F),Lista)
-                break
+print("Digite um número")
+y = int(input(">>> "))
+x = 0
+print(x**2)
+while True:
+    x = x + 1
+    if x < y:
+        print(x**2)
+    if x ==y:
+        print(x**2)
+        break
